3d8aa2048b03385af82d66fa8b57f54963ade169
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-26 00:45:48 +0530

    added firebase methods for sign in and sign up along with validations for user inputs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,74 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    if (message) return;
+
+    if (!isSignInForm) {
+      // Sign Up Logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/12824231?v=4",
+          })
+            .then(() => {
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setErrorMessage(error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      // Sign In Logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
+  };
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -17,13 +83,17 @@ const Login = () => {
           alt="app background"
         />
       </div>
-      <form className="w-3/12 h-auto absolute p-8 bg-black my-24 mx-auto right-0 left-0 text-white rounded-xl bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
         <h1 className="font-medium text-3xl py-2">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
 
         {!isSignInForm && (
           <input
+            ref={name}
             type="text"
             placeholder="Fullname"
             className="px-4 py-2 my-2 w-full bg-gray-700 rounded-xl"
@@ -31,16 +101,22 @@ const Login = () => {
         )}
 
         <input
+          ref={email}
           type="text"
           placeholder="Email"
           className="px-4 py-2 my-2 w-full bg-gray-700 rounded-xl"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="px-4 py-2 my-2 w-full bg-gray-700 rounded-xl"
         />
-        <button className="p-2 my-3 bg-red-700 w-full rounded-xl">
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-2 cursor-pointer" onClick={toggleSignInForm}>
