4962339383e045c6d3a1272443b5116649f2cf68
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-30 00:13:09 +0530

    added IMG_CDN_URL constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,3 +12,5 @@ export const API_OPTIONS = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZmYyZGIxMjg1M2Y5MWYzNmNhM2QyNGZmODJkNjUxNyIsInN1YiI6IjY1YjdlY2E1OGNmY2M3MDE0OGU1MTgxZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fNQts1OdA39lHupKLlEH3dy8xAlfXg7EkZckFh41e0g",
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
