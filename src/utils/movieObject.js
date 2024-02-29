0f26b30481b708faa5996886523721f3c0d6c491
Pranav Jadhav <trinitypranav@gmail.com>
2024-02-29 19:16:26 +0530

    default movie object

---
+++ src/utils/movieObject.js
@@ -0,0 +1,17 @@
+export const movieObject = {
+  adult: false,
+  backdrop_path: "/pWsD91G2R1Da3AKM3ymr3UoIfRb.jpg",
+  genre_ids: [878, 28, 18],
+  id: 933131,
+  original_language: "ko",
+  original_title: "황야",
+  overview:
+    "After a deadly earthquake turns Seoul into a lawless badland, a fearless huntsman springs into action to rescue a teenager abducted by a mad doctor.",
+  popularity: 1382.144,
+  poster_path: "/zVMyvNowgbsBAL6O6esWfRpAcOb.jpg",
+  release_date: "2024-01-26",
+  title: "Badland Huntersasdasdasd",
+  video: false,
+  vote_average: 6.74,
+  vote_count: 441,
+};
