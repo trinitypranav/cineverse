cc9c2cb1cc70a378bab2980f91aa169f026a0e40
Pranav Jadhav <trinitypranav@gmail.com>
2024-03-01 17:35:58 +0530

    dark theme setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import { Provider } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
+import { useEffect } from "react";
+import { setDark } from "./utils/darkSlice";
 
 function App() {
+  const darkTheme = useSelector((state) => state.dark.dark);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (window.localStorage.getItem("DarkTheme") !== null) {
+      dispatch(setDark(window.localStorage.getItem("DarkTheme")));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkTheme === true) {
+      document.getElementById("html").classList.add("dark");
+      dispatch(setDark(darkTheme));
+    } else {
+      document.getElementById("html").classList.remove("dark");
+      dispatch(setDark(darkTheme));
+    }
+
+    window.localStorage.setItem("DarkTheme", JSON.stringify(darkTheme));
+  }, [darkTheme]);
+
   return (
-    <Provider store={appStore}>
-      <Body />
-    </Provider>
+    // <Provider store={appStore}>
+    <Body />
+    // </Provider>
   );
 }
 
