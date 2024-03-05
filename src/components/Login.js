af55f0ff6e0894ae8f3c570ec6338ccb037b2448
Pranav Jadhav <trinitypranav@gmail.com>
2024-03-05 21:34:37 +0530

    using test user credentials for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
 import {
@@ -80,15 +80,20 @@ const Login = () => {
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
+
+  useEffect(() => {
+    email.current.value = "pranav@gmail.com";
+    password.current.value = "Pranav@123";
+  }, []);
   return (
     <div>
       <Header />
-      <div className="absolute">
-        <img src={BG_URL} alt="logo" />
-      </div>
+      {/* <div className="absolute">
+        <img className="h-screen object-cover" src={BG_URL} alt="logo" />
+      </div> */}
       <form
         onSubmit={(e) => e.preventDefault()}
-        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+        className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
       >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
