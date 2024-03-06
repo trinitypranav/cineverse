f756efa1d4472785431a48ec945578568fe50f56
Pranav Jadhav <trinitypranav@gmail.com>
2024-03-06 19:02:11 +0530

    updated code for autosuggestions or typeahead support with memoization

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,16 +1,53 @@
 // import openai from "../utils/openai";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
 import { API_OPTIONS } from "../utils/constants";
 import { addGptMovieResult } from "../utils/gptSlice";
 import OpenAI from "openai";
+import { cacheResults } from "../utils/searchCacheSlice";
 
 const GptSearchBar = () => {
   const dispatch = useDispatch();
   const langKey = useSelector((store) => store.config.lang);
-  const searchText = useRef(null);
+  // const searchText = useRef({});
   const gptKeyText = useRef(null);
+
+  const searchCache = useSelector((state) => state.searchCache);
+  const [searchText, setSearchText] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchCache[searchText]) {
+        console.log("from cache");
+        setSuggestions(searchCache[searchText]);
+      } else {
+        // console.log("from api");
+        getSuggestions(searchText);
+      }
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchText]);
+
+  const getSuggestions = async (searchText) => {
+    if (searchText === "") {
+      setSuggestions([]);
+      return null;
+    }
+    // console.log(process.env.REACT_APP_YOUTUBE_SEARCH_API + searchText);
+    const data = await fetch(
+      process.env.REACT_APP_YOUTUBE_SEARCH_API + searchText
+    );
+    const response = await data.json();
+    // console.log("setting suggestions to ", response);
+    setSuggestions(response);
+    dispatch(cacheResults({ [searchText]: response }));
+  };
 
   // search movie in TMDB
   const searchMovieTMDB = async (movie) => {
@@ -41,7 +78,7 @@ const GptSearchBar = () => {
 
     const gptQuery =
       "Act as a Movie Recommendation system and suggest some movies for the query : " +
-      searchText.current.value +
+      searchText +
       ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
 
     const gptResults = await openai.chat.completions.create({
@@ -75,31 +112,60 @@ const GptSearchBar = () => {
   };
 
   return (
-    <div className="pt-[35%] md:pt-[10%] flex justify-center">
-      <form
-        className="w-full md:w-2/3 bg-gray-800 grid grid-cols-12 rounded-lg"
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <input
-          ref={searchText}
-          type="text"
-          className="p-4 my-4 ml-2 col-span-7 font-extralight text-xs rounded-lg"
-          placeholder={lang[langKey].gptSearchPlaceholder}
-        />
-        <input
-          ref={gptKeyText}
-          type="text"
-          className="p-4 my-4 mx-2 col-span-3 font-extralight text-xs rounded-lg"
-          placeholder="GPT Key"
-        />
-        <button
-          className="col-span-2 my-4 mr-2 py-2 px-2 bg-red-700 text-white rounded-lg text-sm"
-          onClick={handleGptSearchClick}
+    <>
+      <div className="pt-[35%] h-auto md:pt-[10%] flex justify-center">
+        <form
+          className="w-full md:w-2/3 bg-gray-800 grid grid-cols-12 rounded-lg"
+          onSubmit={(e) => e.preventDefault()}
         >
-          {lang[langKey].search}
-        </button>
-      </form>
-    </div>
+          <input
+            // ref={searchText}
+            type="text"
+            className="p-4 my-4 ml-2 col-span-7 font-extralight text-xs rounded-lg"
+            placeholder={lang[langKey].gptSearchPlaceholder}
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            // onBlur={() => setShowSuggestions(false)}
+          />
+          <input
+            ref={gptKeyText}
+            type="text"
+            className="p-4 my-4 mx-2 col-span-3 font-extralight text-xs rounded-lg"
+            placeholder="GPT Key"
+          />
+          <button
+            className="col-span-2 my-4 mr-2 py-2 px-2 bg-red-700 text-white rounded-lg text-sm"
+            onClick={handleGptSearchClick}
+          >
+            {lang[langKey].search}
+          </button>
+        </form>
+      </div>
+      <div>
+        {showSuggestions && suggestions.length > 0 && (
+          <div className="w-60 ml-60 bg-white p-5 border">
+            <ul>
+              {suggestions.map((s) => {
+                return (
+                  <div
+                    key={s}
+                    className="py-1 cursor-pointer"
+                    onClick={(e) => {
+                      console.log(s);
+                      setSearchText(s);
+                      setShowSuggestions(false);
+                    }}
+                  >
+                    🔍 {s}
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 export default GptSearchBar;
